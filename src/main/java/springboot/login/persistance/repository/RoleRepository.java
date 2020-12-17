d3576ad41c319d07b30f02f3d956a643cb7b0dd8
Rumen Rashev <57558028+rumenrashev@users.noreply.github.com>
2020-12-17 18:02:04 +0200

    set authorities

--- src/main/java/springboot/login/persistance/repository/RoleRepository.java
+++ src/main/java/springboot/login/persistance/repository/RoleRepository.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Repository;
 import springboot.login.persistance.entities.Role;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
 
     Optional<Role> getByAuthority(String authority);
+
+    Set<Role> getAllBy();
 
 }
--- src/main/java/springboot/login/service/impl/UserRegisterServiceImpl.java
+++ src/main/java/springboot/login/service/impl/UserRegisterServiceImpl.java
@@ -3,11 +3,14 @@ package springboot.login.service.impl;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import springboot.login.constants.Roles;
 import springboot.login.persistance.entities.User;
 import springboot.login.persistance.repository.RoleRepository;
 import springboot.login.persistance.repository.UserRepository;
 import springboot.login.service.UserRegisterService;
 import springboot.login.service.models.UserRegisterModel;
+
+import java.util.Set;
 
 @Service
 public class UserRegisterServiceImpl implements UserRegisterService {
@@ -31,6 +34,11 @@ public class UserRegisterServiceImpl implements UserRegisterService {
     public String registerUser(UserRegisterModel userRegisterModel) {
         User user = this.modelMapper.map(userRegisterModel, User.class);
         user.setPassword(this.bCryptPasswordEncoder.encode(userRegisterModel.getPassword()));
+        if (this.userRepository.count() == 0) {
+            user.setAuthorities(this.roleRepository.getAllBy());
+        } else {
+            user.setAuthorities(Set.of(this.roleRepository.getByAuthority(Roles.USER.toString()).get()));
+        }
         this.userRepository.saveAndFlush(user);
         return user.getUsername();
     }
