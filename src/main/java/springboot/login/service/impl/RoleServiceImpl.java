2af2b74f16521081462700f6241cb934a65a10f5
Rumen Rashev <57558028+rumenrashev@users.noreply.github.com>
2020-12-17 17:18:47 +0200

    create RoleServiceImpl

--- src/main/java/springboot/login/service/impl/RoleServiceImpl.java
+++ src/main/java/springboot/login/service/impl/RoleServiceImpl.java
@@ -2,8 +2,12 @@ package springboot.login.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import springboot.login.constants.Roles;
+import springboot.login.persistance.entities.Role;
 import springboot.login.persistance.repository.RoleRepository;
 import springboot.login.service.RoleService;
+
+import java.util.EnumSet;
 
 @Service
 public class RoleServiceImpl implements RoleService {
@@ -13,5 +17,18 @@ public class RoleServiceImpl implements RoleService {
     @Autowired
     public RoleServiceImpl(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
+        this.seedRoles();
+    }
+
+    private void seedRoles(){
+        EnumSet<Roles> roles = EnumSet.allOf(Roles.class);
+        roles
+                .stream()
+                .map(Enum::toString)
+                .forEach(r->{
+                    if (this.roleRepository.getByAuthority(r).isEmpty()){
+                        this.roleRepository.saveAndFlush(new Role(r));
+                    }
+                });
     }
 }
